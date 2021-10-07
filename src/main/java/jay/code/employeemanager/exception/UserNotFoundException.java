318679e598eb22a588dd040bba3e588393bd07de
Jay686 <camglory83@gmail.com>
2021-10-07 00:49:29 -0700

    Initial commit

---
+++ src/main/java/jay/code/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package jay.code.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
